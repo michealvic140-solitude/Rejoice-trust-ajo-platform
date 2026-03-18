a5e73dac8492b1ba0eb8ea2c0370aaecd8372fb5
michealvic140-solitude <michealvic140@gmail.com>
2026-03-18 20:18:56 +0100

    Delete app/layout.ts

--- app/layout.ts
+++
@@ -1,5 +0,0 @@
-{localStorage.getItem("maintenanceMode") === "true" && (
-  <div className="bg-red-600 text-white text-center py-3 font-bold fixed top-[72px] left-0 right-0 z-40">
-    {localStorage.getItem("maintenanceMessage") || "Server Maintenance in progress – please check back soon"}
-  </div>
-)}
